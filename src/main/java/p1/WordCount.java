f10281ac6c69c1426e1e0574b5b632d720fa8ba4
Timothée BORELLE <tim-borelle@protonmail.com>
2018-12-02 23:36:43 +0100

    add import

--- src/main/java/p1/WordCount.java
+++ src/main/java/p1/WordCount.java
@@ -1,5 +1,16 @@
 package p1;
 
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+
 public class WordCount {
+	
+	
 
 }
